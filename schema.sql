81c3f5de1186d85bf50966ea455f12851645f600
Nick Huber <nick@nickhuber.ca>
2023-03-07 21:55:01 -0800

    send pg_notify from tunnel application instead of db, 10x performance

--- schema.sql
+++ schema.sql
@@ -2,22 +2,4 @@ CREATE TABLE raw_packet (
   id SERIAL PRIMARY KEY,
   payload BYTEA,
   sender INET,
-  received INT DEFAULT 0
 );
-
-
-CREATE OR REPLACE FUNCTION notify_raw_packet_ready()
-  RETURNS TRIGGER AS $$
-DECLARE
-    row RECORD;
-BEGIN
-  row = NEW;
-  PERFORM pg_notify('raw_packet_ready', '' || row.id);
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE TRIGGER "raw_notify"
-AFTER INSERT ON raw_packet
-FOR EACH ROW EXECUTE PROCEDURE notify_raw_packet_ready();
--- tunnel/main.go
+++ tunnel/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
@@ -28,79 +29,75 @@ const RAW_INSERT_QUERY = `
 INSERT INTO raw_packet (payload, sender) VALUES ($1, $2)
 RETURNING id
 `
+const RAW_NOTIFY_QUERY = `
+SELECT pg_notify('raw_packet_ready:%s', '%d')
+`
 
 func configureInterface(ifce_name string) {
-	ip_addr_cmd := exec.Command("ip", "addr", "add", os.Getenv("TUN_IP"), "dev", ifce_name)
+	ip_addr_cmd := exec.Command("ip", "addr", "add", fmt.Sprintf("%s/30", os.Getenv("TUN_IP")), "dev", ifce_name)
 	ip_addr_cmd.Run()
 	ip_link_cmd := exec.Command("ip", "link", "set", "up", "dev", ifce_name)
 	ip_link_cmd.Run()
 }
 
-func pollTunPackets(ifce *water.Interface, dbPool *pgxpool.Pool) {
+func pollTunPackets(ctx context.Context, ifce *water.Interface, dbPool *pgxpool.Pool) {
 	localIP := os.Getenv("TUN_IP")
-	packet := make([]byte, 2000)
 	for {
+		packet := make([]byte, 2000)
 		n, err := ifce.Read(packet)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if packet[0] != 0x45 {
-			log.Printf("Skipping non-IP packet")
-			continue
-		}
-
-		_, err = dbPool.Exec(context.Background(), RAW_INSERT_QUERY, packet[:n], localIP)
-		if err != nil {
-			log.Fatal(err)
-		}
+		go handlePacketRead(ctx, packet[:n], localIP, dbPool)
 	}
 }
 
-func postgresListen(listen_conn *pgx.Conn, dbPool *pgxpool.Pool, ifce *water.Interface) {
-	var packet []byte
+func handlePacketRead(ctx context.Context, packet []byte, localIP string, dbPool *pgxpool.Pool) {
+	if packet[0] != 0x45 {
+		log.Printf("Skipping non-IPv4 packet")
+		return
+	}
+
+	var id int
+	err := dbPool.QueryRow(ctx, RAW_INSERT_QUERY, packet, os.Getenv("TUN_IP")).Scan(&id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = dbPool.Exec(ctx, fmt.Sprintf(RAW_NOTIFY_QUERY, os.Getenv("TUN_IP"), id))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handlePostgresListen(row_id string, ctx context.Context, dbPool *pgxpool.Pool, ifce *water.Interface) {
+	var payload []byte
+	err := dbPool.QueryRow(ctx, "SELECT payload FROM raw_packet WHERE id = $1", row_id).Scan(&payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = ifce.Write(payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func postgresListen(ctx context.Context, dbPool *pgxpool.Pool, ifce *water.Interface) {
 	peer := os.Getenv("TUN_PEER")
-	listen_conn.Exec(context.Background(), "listen raw_packet_ready")
+	listen_conn, err := dbPool.Acquire(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listen_conn.Release()
+	listen_cmd := fmt.Sprintf("listen \"raw_packet_ready:%s\"", peer)
+	listen_conn.Exec(ctx, listen_cmd)
 	for {
-		// TODO: this should use a connection from the pool but the function doesn't
-		// seem defined for those types of connections ¯\_(ツ)_/¯
-		_, err := listen_conn.WaitForNotification(context.Background())
+		notification, err := listen_conn.Conn().WaitForNotification(ctx)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		rows, _ := dbPool.Query(
-			context.Background(),
-			"SELECT id, payload, length(payload) FROM raw_packet WHERE host(sender) = $1 AND received = 0 LIMIT 50",
-			peer,
-		)
-		defer rows.Close()
-
-		counter := 0
-		for rows.Next() {
-			counter++
-
-			var id int
-			var length int
-			err := rows.Scan(
-				&id,
-				&packet,
-				&length,
-			)
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err = ifce.Write(packet[:length])
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err = dbPool.Exec(context.Background(), "UPDATE raw_packet SET received = 1 WHERE id = $1", id)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		if counter > 1 {
-			log.Printf("%d\n", counter)
-		}
+		handlePostgresListen(notification.Payload, ctx, dbPool, ifce)
 	}
 
 }
@@ -108,6 +105,7 @@ func postgresListen(listen_conn *pgx.Conn, dbPool *pgxpool.Pool, ifce *water.Int
 func main() {
 	ifce := makeTun("postgres-vpn")
 	log.Printf("Interface Name: %s\n", ifce.Name())
+	log.Printf("Interface IP: %s\n", os.Getenv("TUN_IP"))
 	configureInterface(ifce.Name())
 
 	database_url := os.Getenv("DATABASE_URL")
@@ -123,7 +121,8 @@ func main() {
 	}
 	defer listen_conn.Close(context.Background())
 
-	go pollTunPackets(ifce, dbPool)
-	postgresListen(listen_conn, dbPool, ifce)
+	ctx := context.Background()
+	go pollTunPackets(ctx, ifce, dbPool)
+	postgresListen(ctx, dbPool, ifce)
 
 }
